bf90baaa3cbf506fa826c9d7f5a7ef6ad7d4758c
xssdro <85429129+xssdro@users.noreply.github.com>
2021-06-13 19:44:02 +0700

    Update start.bat

--- files/start.bat
+++ files/start.bat
@@ -3,12 +3,9 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By ExDeus Arch" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-
-
 echo =============================
 echo Task Completed, if your RDP offline or dead you can try re-build it again.
