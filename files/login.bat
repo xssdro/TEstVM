1d86eee60d4e09ae8d25c08b6d207aab6d9038a6
xssdro <85429129+xssdro@users.noreply.github.com>
2021-06-13 19:47:49 +0700

    Update login.bat

--- files/login.bat
+++ files/login.bat
@@ -12,7 +12,7 @@ echo .
 echo .
 echo ========= IP Address =========
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo ======================
+echo ==============================
 echo .
 echo .
 echo .
